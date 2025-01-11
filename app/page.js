f85af0d5250000c78240087353a210fc7243d485
TechSubham <subhamchauhan523@gmail.com>
2025-01-11 09:18:22 +0530

    button added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,18 @@
-import Image from "next/image";
-
-export default function Home() {
+"use client"
+import React from 'react'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+const page = () => {
   return (
-    <div className="text-white">
-      this is gdsc project
+    <div className='text-white flex flex-col'>
+      Login and signup code 
+      <Link href="/Homepage">
+      <Button className="bg-red-500 ">
+        Homepage
+      </Button>
+      </Link>
     </div>
-  );
+  )
 }
+
+export default page
